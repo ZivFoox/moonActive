342e1e516f777c0a95521ed65a2bbb7896f83df8
Ziv <zivfoox@gmail.com>
2019-01-04 15:51:40 +0200

    Redis hash demo

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Assignment for ActiveMoon
 ## Version 1.0.1
-### To install Redis in Windows: https://github.com/MicrosoftArchive/redis/releases
+### To install and run Redis in Windows: 
+#### Download latest version https://github.com/MicrosoftArchive/redis/releases
+####Go to C:\Program Files\Redis and run redis-cli.exe
--- redisDemo.js
+++ redisDemo.js
@@ -2,7 +2,7 @@
 var redis = require('redis');
 
 module.exports = {
-    initRedis: function(){
+    initRedis: async function(){
     
         var client = redis.createClient(); // this creates a new client
         client.on('connect', function() {
@@ -13,15 +13,21 @@ module.exports = {
             console.log('Something went wrong ' + err);
         });
 
-        client.set('my test key', 'my test value', redis.print);
-client.get('my test key', function (error, result) {
-    if (error) {
-        console.log(error);
-        throw error;
-    }
-    console.log('GET result ->' + result);
-});
+        // client.zadd("mySet", 11, "Hello world11");
+        // client.zadd("mySet", 2, "Hello world2");
+        // client.zadd("mySet", 51, "Hello world51");
+        // client.zadd("mySet", 7, "Hello world7");
+        // client.zadd("mySet", 10, "Hello world10");
 
+        client.hmset("ziv","123", "123");
+        client.hmset("ziv", "123", "1234");
+        client.hmget("ziv","123",  function (error, result) {
+            if (error) {
+                console.log(error);
+                throw error;
+            }
+            console.log('GET result FROM HASH->' + result);
+            });
     }
 }
 
